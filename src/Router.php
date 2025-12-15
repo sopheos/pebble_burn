ab2fccbfdb61920f3bce72ea8581808823984952
mathieu <mathieu@sopheos.com>
2025-12-15 16:00:04 +0100

    fix doc

--- src/Router.php
+++ src/Router.php
@@ -14,7 +14,7 @@ class Router
     private static ?Router $instance = null;
 
     /**
-     * @var Route[][]
+     * @var RouteInterface[][]
      */
     private array $routes = [];
     private array $wildcards = [];
